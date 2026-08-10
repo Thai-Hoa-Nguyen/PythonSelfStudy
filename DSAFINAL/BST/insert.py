e99aae270356978ee9f903ecd884df0eb4f7e8f3
Thai Nguyen <thai.vc.nguyen@gmail.com>
2026-08-10 12:58:03 -0400

    added removal

--- DSAFINAL/BST/insert.py
+++ DSAFINAL/BST/insert.py
@@ -51,16 +51,70 @@ class BST:
         self.postorder(node.right)
         print(node.value)
 
+    # Remove a node
+    def remove(self, value):
+        self.root = self._remove(self.root, value)
+
+    def _remove(self, node, value):
+        # Value wasn't found
+        if node is None:
+            return None
+
+        # Search left
+        if value < node.value:
+            node.left = self._remove(node.left, value)
+
+        # Search right
+        elif value > node.value:
+            node.right = self._remove(node.right, value)
+
+        # Found the node
+        else:
+            # Case 1: no children
+            if node.left is None and node.right is None:
+                return None
+
+            # Case 2: only right child
+            if node.left is None:
+                return node.right
+
+            # Case 2: only left child
+            if node.right is None:
+                return node.left
+
+            # Case 3: two children
+            successor = self._find_min(node.right)
+
+            node.value = successor.value
+
+            node.right = self._remove(
+                node.right,
+                successor.value
+            )
+
+        return node
+
+    # Helper function
+    def _find_min(self, node):
+        while node.left is not None:
+            node = node.left
+
+        return node
+
+
+# Create BST
 bst = BST()
 
+# Insert values
 for value in [50, 30, 70, 20, 40, 60, 80]:
     bst.insert(value)
 
+# Traversals
 print("Inorder:")
 bst.inorder(bst.root)
 
-print("Preorder:")
+print("\nPreorder:")
 bst.preorder(bst.root)
 
-print("Postorder:")
+print("\nPostorder:")
 bst.postorder(bst.root)
