b01fdd7fa347e52e07f45648fece1c721badb842
Thai Nguyen <thai.vc.nguyen@gmail.com>
2026-08-17 17:24:35 -0400

    aggregate function

---
+++ learnPython/aggregation/aggregate.py
@@ -0,0 +1,27 @@
+import pandas as pd
+
+df = pd.read_csv("data.csv")
+
+#Whole data Frame
+# print(df.mean(numeric_only=True))
+# print(df.sum(numeric_only=True))
+# print(df.min(numeric_only=True))
+# print(df.max(numeric_only=True))
+# print(df.count())
+
+#Single Column
+# print(df["Height"].mean())
+# print(df["Height"].sum())
+# print(df["Height"].min())
+# print(df["Height"].max())
+# print(df["Height"].count())
+
+#Group by
+# group = df.groupby("Type1")
+# print(group["Height"].mean())
+
+group = df.groupby("Type1")
+
+# print(group["Height"].min())
+# print(group["Height"].max())
+print(group["Height"].count())
